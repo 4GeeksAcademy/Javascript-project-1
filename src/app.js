ed43faa1304564d05a684b73075d230525725b61
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-05-29 02:05:23 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoindx = Math.floor(Math.random() * who.length);
+  let actionindx = Math.floor(Math.random() * action.length);
+  let whatindx = Math.floor(Math.random() * what.length);
+  let whenindx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindx] +
+    " " +
+    action[actionindx] +
+    " " +
+    what[whatindx] +
+    " " +
+    when[whenindx]
+  );
 };
